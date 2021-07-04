607e2f1a040f22f24c4fd71d935835d7be8457d0
srinathmuthusamy <86880223+srinathmuthusamy@users.noreply.github.com>
2021-07-04 12:49:50 +0530

    A major update

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// const btn = document.querySelector(".talk");
+// const content = document.querySelector(".content");
+// const play = document.querySelector("#three");
+
+// const greeting = ["I am fine thank you", "You are my king", "Hey My friend"];
+// const date = `The  is ${new Date()} Hope i helped you`;
+// const time = `The time is ${}`
+
+// const SpeechRecognition =
+//   window.SpeechRecognition || window.webkitSpeechRecognition;
+// const recognition = new SpeechRecognition();
+
+// recognition.onstart = function () {
+//   console.log("voice is activated, you can speak to microphoneee");
+// };
+// recognition.onresult = function (event) {
+//   const current = event.resultIndex;
+//   const transcript = event.results[current][0].transcript;
+//   content.textContent = transcript;
+//   readOutLoud(transcript);
+// };
+
+// //add the Listener to the btn
+
+// btn.addEventListener("click", () => {
+//   recognition.start();
+// });
+
+// function readOutLoud(message) {
+//   const speech = new SpeechSynthesisUtterance();
+//   speech.text = message;
+//   speech.volume = 1;
+//   speech.rate = 0.8;
+//   speech.pitch = 1;
+//   window.speechSynthesis.speak(speech);
+// }
+// document.querySelector("#two").addEventListener("click", function () {
+//   content.textContent = "";
+// });
+// const copyBtn = document.querySelector("#one");
+// copyBtn.addEventListener("click", function copy() {
+//   let range = document.createRange();
+//   console.log(range);
+//   range.selectNode(content);
+//   window.getSelection().removeAllRanges();
+//   window.getSelection().addRange(range);
+// });
+  
 const btn = document.querySelector(".talk");
 const content = document.querySelector(".content");
 const play = document.querySelector("#three");
@@ -9,6 +57,9 @@ const how = "I was created with HTML CSS and JavaScript"
 const answer = "Elon Musk is the owner of tesla"
 const answer2 = "Sundar Pichai is the CEO of google"
 const ytopen = "Opening youtube"
+const ytopenwl = "Opening Your watch later video";
+const facebook = "Opening facebook"
+const openWhatsapp = "Opening whatsapp"
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -23,35 +74,50 @@ recognition.onresult = function (event) {
   content.textContent = transcript;
   readOutLoud(transcript);
 };
-
 //add the Listener to the btn
 
 btn.addEventListener("click", () => {
   recognition.start();
 });
-
 function readOutLoud(message) {
   const speech = new SpeechSynthesisUtterance();
   speech.text = message;
-  if(message.includes("today's date")){
-    speech.text = date;
+  if(message.includes("What's today's date")){
+    speech.text = date
   }
-  if(message.includes("who created you")){
-    speech.text = who;
-  }
-  if(message.includes("how you are built")){
-    speech.text = how;
-  }
-  if(message.includes("who is the owner of Tesla")){
-    speech.text = answer;
-  }
-  if(message.includes("who is the CEO of Google")){
-    speech.text = answer2;
-  }
-  if(message.includes("open YouTube")){
-    window.open("https://youtube.com", "_blank")
-    speech.text = ytopen
-  }
+    if (message.includes("who created you")) {
+      speech.text = who;
+    }
+    if (message.includes("how you are built")) {
+      speech.text = how;
+    }
+    if (message.includes("who is the owner of Tesla")) {
+      speech.text = answer;
+    }
+    if (message.includes("who is the CEO of Google")) {
+      speech.text = answer2;
+    }
+    if (message.includes("open YouTube")) {
+      window.open("https://youtube.com", "_blank");
+      speech.text = ytopen;
+    }
+    if (message.includes("open my Watch Later video")){
+      window.open(
+        "https://www.youtube.com/watch?v=377AQ0y6LPA&ab_channel=JavaScriptMastery",
+        "_blank"
+      );
+      speech.text = ytopenwl;
+    }
+    if (message.includes("open Facebook")){
+      window.open(
+        "https://www.facebook.com", "_blank"
+      )
+      speech.text = facebook;
+    }
+        if (message.includes("open WhatsApp")) {
+          window.open("https://web.whatsapp.com");
+          speech.text = openWhatsapp;
+        }
   speech.volume = 1;
   speech.rate = 0.8;
   speech.pitch = 1;
@@ -68,4 +134,4 @@ copyBtn.addEventListener("click", function copy() {
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(range);
 });
-//Done by Srinath.Muthusamy 
+
