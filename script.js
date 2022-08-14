0899564f64b23f3a8c7aa282f8f0c0e843b9309c
srinathmuthusamy <86880223+srinathmuthusamy@users.noreply.github.com>
2022-08-14 21:55:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ function readOutLoud(message) {
       document.body.style.background = "black";
       speech.text = darkMode;
     }
-  if(message.includes("search Youtube for")){
+  if(message.includes("search YouTube for")){
      window.open(`https://www.youtube.com/results?search_query=${message.splice(20)}`)
    }
   if(message.includes("what is your name")){
