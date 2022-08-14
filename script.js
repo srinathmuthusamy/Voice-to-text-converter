57878e04293786e36e0d3f26a74db4fbb33fb915
srinathmuthusamy <86880223+srinathmuthusamy@users.noreply.github.com>
2022-08-14 22:02:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function readOutLoud(message) {
       speech.text = darkMode;
     }
   if(message.includes("search youtube for")){
-     window.open(`https://www.youtube.com/results?search_query=${message}`)
+     window.open(`https://www.youtube.com/results?search_query=${message.splice(19)}`)
    }
   if(message.includes("what is your name")){
        speech.text = 'My Name is DRACX'
